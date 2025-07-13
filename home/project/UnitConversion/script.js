ebf83bef8b3add52b77d5aa86f8a9d791f84ab6f
Prakhar Srivastava <prakharbksc9@gmail.com>
2025-07-13 23:11:41 +0530

    added todolist

---
+++ home/project/UnitConversion/script.js
@@ -0,0 +1,15 @@
+function temperature(){
+    var c= document.getElementById("celsius").value;
+    var f= (c*9/5) +32;
+    document.getElementById("fahrenheit").value= f;
+}
+function weight(){
+    var kg=document.getElementById("kilo").value;
+    var p=kg*2.2;
+    document.getElementById("pounds").value=p;
+}
+function distance(){//Distance(Miles) = Distance(Kms) * 0.62137
+    var k = document.getElementById("km").value;
+    var m = k * 0.62137;
+    document.getElementById("miles").value =m;
+}
