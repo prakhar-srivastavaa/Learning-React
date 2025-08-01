116d64ead43e98223630641e9200708c19f4ee6b
Prakhar Srivastava <prakharbksc9@gmail.com>
2025-08-01 12:22:00 +0530

    added list and function for delete Heding button

--- home/project/todo_list/src/Components/TodoList.jsx
+++ home/project/todo_list/src/Components/TodoList.jsx
@@ -11,6 +11,22 @@ const TodoList = () => {
       setHeadingInput('');
     }
   };
+  const handleAddList =(index) => {
+    if(listInputs[index] && listInputs[index].trim() !== ''){
+      const newTodos = [...todos];
+      newTodos[index].lists.push(listInputs[index]);
+      setTodos(newTodos);
+      setListInputs({...listInputs, [index]: ''});
+    }
+  };
+  const handleListInputChange = (index, value) => {
+    setListInputs({...listInputs, [index]: value});
+  };
+  const handleDeleteTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
 
   return (
     <>
@@ -32,6 +48,27 @@ const TodoList = () => {
             <div className='heading_todo'>
               <h3>{todo.heading}</h3>
               <button className="delete-button-heading" onClick={() => handleDeleteTodo(index)}>Delete Heading </button>
+              {/* <button className="delete-button-heading" onClick={handleDeleteTodo}>Delete Heading</button> */}
+            </div>
+               <ul>
+                {/* Iterate over each list item inside the current todo */}
+                {todo.lists.map((list, listIndex) => (
+                  <li key={listIndex} className='todo_inside_list'>
+                    {/* Display the text content of the list item */}
+                    <p>{list}</p>
+                  </li>
+                ))}
+              </ul>
+            <div className="add_list">
+
+              <input type="text" 
+                     className="list-input"
+                     placeholder='Add List'
+                     value={listInputs[index]||''}
+                     onChange={(e)=> handleListInputChange(index, e.target.value)}/>
+              <button className="add-list-button" onClick={() => handleAddList(index)}>Add List</button>
+
+
             </div>
           </div>
         ))}
